4f8aca19563617cf571c824b38ee3ae43ba2138c
Hari Prakash J <55597500+hari251@users.noreply.github.com>
2024-09-12 12:01:54 +0530

    Add files via upload

---
+++ src/main/java/com/acme/App.java
@@ -0,0 +1,14 @@
+package com.acme;
+
+public class App {
+    public String getGreeting() {
+		//System.out.println(new App().getGreeting());
+        return "Hello world.";
+		
+    }
+
+    public static void main(String[] args) {
+	//System.out.println(new App().getGreeting());
+        System.out.println(new App().getGreeting());
+    }
+}
